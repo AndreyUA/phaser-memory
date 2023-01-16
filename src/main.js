497e9b20bf7ca52094e82f396a6ea0cefba5ceaa
AndreyUA <infernalsteel666@gmail.com>
2023-01-16 21:38:17 +0200

    basic phaser project configuration and added background file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import * as phaser from "phaser";
+import * as Phaser from "phaser";
 
-console.log("hello world");
+const scene = new Phaser.Scene("Game");
+scene.preload = function () {
+  console.log("preload method");
+};
+scene.create = function () {
+  console.log("create method");
+};
 
-console.log(phaser);
+const config = {
+  type: Phaser.AUTO, // webgl (default) OR canvas
+  width: 1280,
+  height: 720,
+  scene,
+};
+
+const game = new Phaser.Game(config);
